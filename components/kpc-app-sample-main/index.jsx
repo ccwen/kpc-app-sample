56753aaee396903143e3ebff8f1df5bcf70eeb4b
yap cheah shen <yapcheahshen@gmail.com>
2013-12-23 22:03:09 +0800

    first demo of kse

---
+++ components/kpc-app-sample-main/index.jsx
@@ -0,0 +1,30 @@
+/** @jsx React.DOM */
+var queryinput=Require('query-input');
+var resultlist=Require('result-list');
+var searchworker=Require('search-worker');
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return {query: "this", db:"search3",result:{texts:[]}};   
+  },
+  render: function() {
+    return ( 
+    	<div> 
+        <queryinput query={this.state.query}/>
+        <searchworker db={this.state.db} query={this.state.query}/>
+        <resultlist result={this.state.result}/>
+      </div>
+    ); 
+  },
+  queryChange:function(data) {
+      this.setState({query:data});
+  },
+  resultready:function(data) {
+      this.setState({result:data});
+  },
+  componentWillMount:function() {
+    mediator.subscribe("query.change",this.queryChange.bind(this));
+    mediator.subscribe("result.ready",this.resultready.bind(this));
+  }
+});
+module.exports=Main;
--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-React=require('react');
-Require=function(arg){return require("../"+arg)};
-var main=require('main')
-React.renderComponent(main(),document.getElementById("main"));
+require('boot');
