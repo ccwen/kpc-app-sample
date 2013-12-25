a91beec563bdf544822cd53e0505525a791024c3
yap cheah shen <yapcheahshen@gmail.com>
2013-12-26 01:38:00 +0800

    make code simpler for tutorial

--- components/kpc-app-sample-query-input/index.jsx
+++ components/kpc-app-sample-query-input/index.jsx
@@ -1,16 +1,19 @@
 /** @jsx React.DOM */
 var QueryInput = React.createClass({
-  getDefaultProps:function() {
-    return { onQueryChange:function(){} };
+  propTypes: {
+    //list of PropTypes  http://facebook.github.io/react/docs/reusable-components.html
+    onQueryChange: React.PropTypes.func.isRequired
   },
   onChange:function(e) {
     if (this.timer) clearTimeout(this.timer);
+
+    var handleInput=this.handleInput.bind(this);
+    //if no input after 0.4 second, run the callback
+    this.timer=setTimeout(handleInput,400); 
+  },
+  handleInput:function() {
     var newquery=this.refs.inputquery.getDOMNode().value;
-    this.timer=setTimeout(
-      (function(){
-        this.props.onQueryChange(newquery);
-      }).bind(this) 
-    ,400); //if no input after 0.4 second, run the callback
+    this.props.onQueryChange(newquery);
   },
   render: function() {
     return ( // the input box
